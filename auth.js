0f967bd118de5857922a0e4feaf8dd43f110ae9b
Marcella Hastings <marcellahastings@gmail.com>
2017-07-02 18:01:13 -0400

    add small hashing website

---
+++ auth.js
@@ -0,0 +1,17 @@
+function hash() {
+    document.getElementById("hash").innerHTML = 
+         hashcode(document.getElementById("msg_form").value);
+}
+
+// NOT A SECURE HASH FUNCTION 
+// DO NOT USE
+function hashcode(str){
+    var hash = 0;
+    if (str.length == 0) return hash;
+    for (i = 0; i < str.length; i++) {
+        char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
